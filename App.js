694a0f8f320db42c3c0e817263f86b18c1a8ffc1
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-01 23:12:05 +0700

    layout2

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Layout1 from './components/layout1';
-// import Layout2 from './components/layout2';
+// import Layout1 from './components/layout1';
+ import Layout2 from './components/layout2';
 // import Layout3 from './components/layout3';
 // import Layout4 from './components/layout4';
 // import Layout5 from './components/layout5';
@@ -11,8 +11,8 @@ import Layout1 from './components/layout1';
 
 export default function App() {
   return (
-     <Layout1/>
-    // <Layout2/>
+     //<Layout1/>
+     <Layout2/>
     // <Layout3/>
     // <Layout4/>
     // <Layout5/>
